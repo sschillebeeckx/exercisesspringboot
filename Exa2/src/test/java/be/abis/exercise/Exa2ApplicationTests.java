95329ed7931333d8c33feb05920dc7a97256dfd5
Sandy <sschillebeeckx@abis.be>
2021-10-18 14:10:06 +0200

    blabla

---
+++ Exa2/src/test/java/be/abis/exercise/Exa2ApplicationTests.java
@@ -0,0 +1,13 @@
+package be.abis.exercise;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class Exa2ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
